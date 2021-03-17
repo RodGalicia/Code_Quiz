3e1ad37ac63b3945061101a0ea36892918a055e3
Rodrigo Galicia <rodrigo_galicia95@yahoo.com>
2021-03-17 17:47:51 -0500

    got the timer to work

--- script.js
+++ script.js
@@ -10,10 +10,44 @@ var questions = [
     {
         question: "Which company developed JavaScript?",
         choices: ["Red Hat", "Blizzard", "Netscape", "HP"],
-        answer: "true and false",
+        answer: "Netscape",
         userAnswer: "",
         outcome: false,
         time: 0
     },
 
 ];
+
+function startBtn() {
+var myVar = setInterval(myTimer, 1000);
+    
+
+};
+var x = 60;
+
+function myTimer() {
+
+document.getElementById("timer").innerHTML = x;
+
+if(x == 0) {
+
+clearInterval(myVar);
+
+} else if (x > 0) {
+
+x--
+
+}};
+
+//try to get my timer to work whenever user clicks on start button.
+    //make a variable for the timer so it counts down to zero.
+        //look into the set interval method.
+        //everytime a wrong answer is selected the timer loses 10 seconds.
+
+
+
+
+
+//put in the choices into each selector button
+//make the scoreboard button work and lets you see your score kept by it.
+//
