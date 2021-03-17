4c1ffadbf4ba9a6c1416f4fb9cc08804d7e2b473
Rodrigo Galicia <rodrigo_galicia95@yahoo.com>
2021-03-16 22:29:17 -0500

    First commit of the Quiz game, HTML and CSS working how it should

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Code_Quiz
+This is the first try of making a Quiz game, I ran out of time but plan on coming back to it until I get a better understanding of Javascript, for now My CSS and my HTML work perfect!.
---
+++ script.js
@@ -0,0 +1,19 @@
+var questions = [
+    {
+        question: "Who created JavaScript?",
+        choices: ["Brendan Eich", "Bill Gates", "Dave Shea", "John Resig"],
+        answer: "Brendan Eich",
+        userAnswer: "",
+        outcome: false,
+        time: 0
+    },
+    {
+        question: "Which company developed JavaScript?",
+        choices: ["Red Hat", "Blizzard", "Netscape", "HP"],
+        answer: "true and false",
+        userAnswer: "",
+        outcome: false,
+        time: 0
+    },
+
+];
